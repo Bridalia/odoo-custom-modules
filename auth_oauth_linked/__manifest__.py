83547071a61615c21f8ea5200c7a261e07db4439
Jason <longvm91@gmail.com>
2024-04-06 21:36:59 +0700

    update description

--- auth_oauth_linked/__manifest__.py
+++ auth_oauth_linked/__manifest__.py
@@ -6,6 +6,8 @@
     "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "Jason Vu",
+    'email': "longvm91@gmail.com",
+    'website': "https://github.com/longvm91/odoo-custom-modules/tree/17.0/auth_oauth_linked",
     "summary": """Allow linked multiple OAuths to account""",
     "category": "Tool",
     "website": "",
@@ -15,6 +17,7 @@
         'views/linked_oauth_templates.xml',
         'views/res_config_settings_views.xml',
     ],
+    'images': ['static/img/allow_unlink.png'],
     'assets': {
         'web.assets_frontend': [
             'auth_oauth_linked/static/src/**/*',
